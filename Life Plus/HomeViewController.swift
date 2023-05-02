40a224aa30f5bb86717920a4dff8db4908d4554f
Habibur Rahman <mhr.habibur@gmail.com>
2023-05-02 17:31:34 +0600

    first commit

---
+++ Life Plus/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life Plus
+//
+//  Created by Habibur Rahman on 5/2/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
